9e050b3feb00574be7bc08cf4348653ac989d64f
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-08 21:09:07 +0800

    Membuat Main Class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String namaDepan = "Syahri";
+        String namaBelakang = "Ramadhan";
+
+        int usia =20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + " " + namaBelakang);
+    }
+}
