e59ece49252bcc0a94968dc4c78fa90947233118
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-08 21:30:31 +0800

    Operasi Tipe String

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,22 @@ public class Main {
         char[]uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("mad"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,10));
+        System.out.println(namaLengkap.replace("Sya","Ri"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(5));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
