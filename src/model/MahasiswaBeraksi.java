b20fa390f9824db48573c2dab7722be0767d1210
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-09 13:57:16 +0800

    Membuat Class, Atribut, dan Objek

---
+++ src/model/MahasiswaBeraksi.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.npm = "19630190";
+        mahasiswa.nama = "Syahri";
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("09-09-2009");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.npm = "19630191";
+        mahasiswa2.nama = "Ramadhan";
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("08-08-2008");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+
+        mahasiswa.tampilkanAtribut();
+        mahasiswa.menyapa();
+        System.out.println("Usiaku : " + mahasiswa.hitungUsia() + " tahun");
+    }
+}
