96f99be6ab9dc4c5776879ab7418881090f06987
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-12 11:58:55 +0800

    Perintah membuat Objek dari sebuah Class (Isntance)

--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -1,43 +1,9 @@
 package model;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Mahasiswa {
     String npm;
     String nama;
     Date tanggalLahir;
-
-    void tampilkanAtribut(){
-        String polaTanggal = "dd-MM-yyyy";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
-
-        System.out.println("Nama \t\t\t : " + this.nama);
-        System.out.println("NPM \t\t\t : " + this.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(this.tanggalLahir));
-
-    }
-
-    void menyapa(){
-        System.out.println("Hai nama aku : " + this.nama);
-    }
-
-    int hitungUsia(){
-        Calendar tanggalLahir = Calendar.getInstance();
-        tanggalLahir.setTime(this.tanggalLahir);
-        Calendar hariIni = Calendar.getInstance();
-
-        int selisihTahun = hariIni.get(Calendar.YEAR) - tanggalLahir.get(Calendar.YEAR);
-
-        if(hariIni.get(Calendar.MONTH) < tanggalLahir.get(Calendar.MONTH) ){
-            selisihTahun--;
-        }else {
-            if(hariIni.get(Calendar.MONTH) == tanggalLahir.get(Calendar.MONTH)
-                    && hariIni.get(Calendar.DAY_OF_MONTH) < tanggalLahir.get(Calendar.DAY_OF_MONTH)){
-                selisihTahun--;
-            }
-        }
-        return selisihTahun;
-    }
 }
