4064e83b61d7a633cb2902af6ebe14159ad7966e
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-12 12:07:12 +0800

    Membuat tipe data Date (SimpleDateFormat dan parse)

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -1,13 +1,19 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19630190";
         mahasiswa.nama = "Syahri";
-        mahasiswa.tanggalLahir = new Date();
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("09-09-2009");
 
         System.out.println("Nama \t\t\t : " + mahasiswa.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa.npm);
@@ -16,10 +22,14 @@ public class MahasiswaBeraksi {
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19630191";
         mahasiswa2.nama = "Ramadhan";
-        mahasiswa2.tanggalLahir = new Date();
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("08-08-2008");
 
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
         System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
     }
 }
